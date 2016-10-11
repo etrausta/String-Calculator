b9cb04945a31115fa2ba8267549a363d1a778eed
Eyþór Traustason <Eyr Traustason>
2016-10-11 14:24:19 +0000

    Added .jar files to get this project started

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,5 +2,7 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text)
+	public static int add(String text) {
+		return -1;
+	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,7 +1,7 @@
 package is.ru.stringcalculator;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.Test
+import org.junit.Test;
 
 public class CalculatorTest {
 
