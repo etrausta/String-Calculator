d44e0eb08b67e19376122d1dabe3c8ab6330eded
Eyþór Traustason <Eyr Traustason>
2016-10-13 09:03:54 +0000

    Added the sum helper function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,12 +6,7 @@ public class Calculator {
 		if(text.equals(""))
 			return 0;
 		else if (text.contains(",")) {
-			String [] numbers = text.split(",");
-			int total = 0;
-			for(String number : numbers) {
-				total += toInt(number);
-			}
-			return total;
+
 		}
 		else 
 			return 1;
@@ -24,5 +19,13 @@ public class Calculator {
 
 	private static String [] splitNumbers(String numbers) {
 		return numbers.split(",");
+	}
+
+	private static int sum (String [] numbers) {
+			int total = 0;
+			for(String number : numbers) {
+				total += toInt(number);
+			}
+			return total;
 	}
 }
